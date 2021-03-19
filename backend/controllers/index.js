aab48c709284ba95e844a1149d06c7b599662744
Andika Adhinata <aa80004@student.uph.edu>
2021-03-19 18:39:30 +0700

    disable database

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -1,21 +1,21 @@
 const Service = require("../services");
 
 const controllers = {
-  getThoughts: async (req, res, next) => {
-    try {
-        const response = await Service.getThoughts();
-        res.send(response);
-    } catch (err) {
-      next(err);
-    }
-  },
+  // getThoughts: async (req, res, next) => {
+  //   try {
+  //       const response = await Service.getThoughts();
+  //       res.send(response);
+  //   } catch (err) {
+  //     next(err);
+  //   }
+  // },
 
   addThought: async (req, res, next) => {
     try {
       const { author, content } = req.body;
-      const response = await Service.addThought(author, content);
-      if (response) req.app.io.emit('new-thought', response);
-      res.send(response);
+      // const response = await Service.addThought(author, content);
+      req.app.io.emit('new-thought', { author, content });
+      res.sendStatus(200);
     } catch (err) {
       next(err);
     }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const Controller = require('../controllers');
 
-router.get('/thoughts', Controller.getThoughts);
+// router.get('/thoughts', Controller.getThoughts);
 router.post('/thoughts', Controller.addThought);
 
 module.exports = router;
